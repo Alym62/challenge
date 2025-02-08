c1d9d80613af4ce4863a49b49d20a965aad00796
Alyasaf Meireles <gasafm62@gmail.com>
2025-02-08 18:46:53 -0300

    feat: login implementado e configuração de cors implementado.

--- backend/src/main/java/com/github/alym62/challenge/backend/infrastructure/security/SecurityConfig.java
+++ backend/src/main/java/com/github/alym62/challenge/backend/infrastructure/security/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +32,22 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/v1/contato/**").authenticated()
                         .requestMatchers("/api/v1/usuario/**").permitAll()
---
+++ frontend/src/app/pages/auth.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { AuthModel } from "@models/auth.model";
+import { api } from "@service/api";
+import { Button, Card, Form, Input, message } from "antd";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const Auth = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [isRegister, setIsRegister] = useState<boolean>(false);
+    const router = useRouter();
+
+    const handleLogin = async (request: AuthModel): Promise<void> => {
+        setLoading(true);
+        try {
+            const { data } = await api.post(`/${isRegister ? 'usuario/create' : 'auth/login'}`, request);
+
+            message.success(isRegister ? 'Registrado com sucesso!' : 'Login realizado com sucesso!');
+            if (!!data && data.accessToken) {
+                localStorage.setItem('accessToken', data.accessToken);
+            }
+
+            // router.push('/home');
+        } catch (error: any) {
+            message.error(error.response?.data?.message || 'Erro ao autenticar');
+        }
+        setLoading(false);
+    }
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <Card className="w-96 shadow-lg">
+                <h2 className="text-center text-xl font-semibold">{isRegister ? "Registrar" : "Login"}</h2>
+                <Form layout="vertical" onFinish={(request: AuthModel) => handleLogin(request)}>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, type: "email", message: "Digite um email válido!" }]}
+                    >
+                        <Input placeholder="Digite seu email" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Senha"
+                        name="senha"
+                        rules={[{ required: true, min: 1, message: "A senha deve ter pelo menos 6 caracteres!" }]}
+                    >
+                        <Input.Password placeholder="Digite sua senha" />
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit" block loading={loading}>
+                        {isRegister ? "Registrar" : "Entrar"}
+                    </Button>
+                </Form>
+                <p className="text-center mt-2">
+                    {isRegister ? "Já tem uma conta?" : "Não tem uma conta?"}{" "}
+                    <span
+                        className="text-blue-500 cursor-pointer"
+                        onClick={() => setIsRegister(!isRegister)}
+                    >
+                        {isRegister ? "Entrar" : "Registrar"}
+                    </span>
+                </p>
+            </Card>
+        </div>
+    );
+}
